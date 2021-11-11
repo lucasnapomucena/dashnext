85c0a594261a86fe7cc80b91d2e35327d8229e64
macbook <macbook@192.168.1.3>
2021-11-11 19:57:32 -0300

    feat(chart): add chart to dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,9 @@
 import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
-import { RiDashboardLine } from "react-icons/ri";
+import {
+  RiContactsLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+} from "react-icons/ri";
 
 export function Sidebar() {
   return (
@@ -7,13 +11,41 @@ export function Sidebar() {
       <Stack spacing={12} align="flex-start">
         <Box>
           <Text fontWeight="bold" color="gray.400" fontSize="small">
-            Geral
+            GERAL
           </Text>
           <Stack spacing="4" mt="8" align="stretch">
-            <Link>
+            <Link display="flex" align="center">
               <Icon as={RiDashboardLine} fontSize="20" />
               <Text ml="4" fontWeight="medium">
                 Dashboard
+              </Text>
+            </Link>
+
+            <Link display="flex" align="center">
+              <Icon as={RiContactsLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Usuários
+              </Text>
+            </Link>
+          </Stack>
+        </Box>
+
+        <Box>
+          <Text fontWeight="bold" color="gray.400" fontSize="small">
+            AUTOMAÇÃO
+          </Text>
+          <Stack spacing="4" mt="8" align="stretch">
+            <Link display="flex" align="center">
+              <Icon as={RiDashboardLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Formulários
+              </Text>
+            </Link>
+
+            <Link display="flex" align="center">
+              <Icon as={RiGitMergeLine} fontSize="20" />
+              <Text ml="4" fontWeight="medium">
+                Automação
               </Text>
             </Link>
           </Stack>
