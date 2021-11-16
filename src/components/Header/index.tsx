bd84acf13dabe66902eb6ac8719ab22539b034cb
lucas napomucena <lucasnapomucena@gmail.com>
2021-11-16 14:59:30 -0300

    feat(header): create responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Icon, Flex, useBreakpointValue, IconButton } from "@chakra-ui/react";
 import { NotificationsNav } from "./NotificationsNav";
 import { SearchBox } from "./SearchBox";
 import { Profile } from "./Profile";
 import { Logo } from "./Logo";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       w="100%"
@@ -16,11 +25,23 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
-      <SearchBox />
+
+      {isWideVersion && <SearchBox />}
+
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
