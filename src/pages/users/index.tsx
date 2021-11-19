5b9caabf43d33c0e736368760af01f7c3a5552dc
Lucas Napomucena <lucasnapomucena@gmail.com>
2021-11-19 13:13:12 -0300

    feat: create handleprefetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,5 @@
+import NextLink from 'next/link';
+
 import {
   Box,
   Flex,
@@ -22,6 +24,8 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from '../../services/queyClient';
+import { api } from '../../services/api';
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -31,6 +35,17 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+ async function handlePrefetchUser(userId: number) {
+await queryClient.prefetchQuery(['user', userId], async () => {
+  const response = await api.get(`users/${userId}`)
+
+  return response.data
+}, {
+  staleTime: 1000 * 60 * 10
+})
+  }
+
   return (
     <Box>
       <Header />
@@ -45,7 +60,7 @@ export default function UserList() {
                 <Spinner size="sm" color="gray.500" ml="4" />
               )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -54,7 +69,7 @@ export default function UserList() {
               >
                 Criar Novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
           {isLoading ? (
             <Flex justify="center">
@@ -89,7 +104,10 @@ export default function UserList() {
 
                         <Td>
                           <Box>
+
+                            <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(Number(user.id))}>
                             <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">
                               {user.email}
                             </Text>
